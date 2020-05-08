5900603283c0653e90f3ecc3024108ece5080c7a
John Nnamchi <john.nnamchi@mail.mcgill>
2020-05-08 14:40:55 -0400

    Added booleans to run-all bash script

--- run-all.sh
+++ run-all.sh
@@ -6,15 +6,26 @@ echo "Javascript Strings"
 node strings/strings.js
 echo
 echo "Python Strings"
-python strings/strings.py
+python3 strings/strings.py
 
 echo
 echo
 
-echo "2. NUMBERS EXERCISES:"
+echo "2. BOOLEANS EXERCISES:"
+echo
+echo "Javascript Booleans"
+node booleans/booleans.js
+echo
+echo "Python Booleans"
+python3 booleans/booleans.py
+
+echo
+echo
+
+echo "3. NUMBERS EXERCISES:"
 echo
 echo "Javascript Numbers"
 node numbers/numbers.js
 echo
 echo "Python Numbers"
-python numbers/numbers.py
+python3 numbers/numbers.py
